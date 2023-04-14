ea9687ea9a4115d4d78080a27370c7156b6c9681
deba <routdebasisha@gmail.com>
2023-04-14 11:53:36 +0530

    Modified application_yml and swagger config

--- src/main/java/com/miniproject/config/SwaggerConfig.java
+++ src/main/java/com/miniproject/config/SwaggerConfig.java
@@ -8,11 +8,14 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 	@Bean
 	public Docket plansReportApiDoc() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.miniproject.restcontroller")).paths(PathSelectors.any()).build();
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.miniproject.restcontroller")).paths(PathSelectors.any())
+				.build();
 	}
 }
